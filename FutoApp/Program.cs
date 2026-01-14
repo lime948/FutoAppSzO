61a3bc06c2a500965ad7c356ffb5bb2645bb4802
lime948 <oreskoz@kkszki.hu>
2026-01-14 18:26:09 +0100

    .

--- FutoApp/Program.cs
+++ FutoApp/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +12,28 @@ namespace FutoApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            WriteLineCentered("=== FUTÓ EDZÉS NAPLÓ ===");
+            WriteLineCentered("1. Regisztráció");
+            WriteLineCentered("2. Bejelentkezés");
+            WriteLineCentered("3. Kilépés");
+            WriteLineCentered("-------------------------");
+            WriteCentered("Válassza ki a menüpontot: ");
+            int menu = int.Parse(Console.ReadLine());
+            switch (menu)
+            {
+                case 1:
+                    // Regisztráció
+                    break;
+                case 2:
+                    // Bejelentkezés
+                    break;
+                case 3:
+                    // Kilépés
+                    break;
+                default:
+                    WriteLineCentered("Érvénytelen menüpont!");
+                    break;
+            }
             //Runner futo = new Runner(
             //    magassag: 178,
             //    testtomeg: 72,
@@ -30,6 +52,27 @@ namespace FutoApp
 
             //Console.WriteLine("Edzések száma: " + futo.Edzesek.Count);
             //Console.WriteLine("Első edzés dátuma: " + futo.Edzesek[0].Datum.ToShortDateString());
+        }
+
+        public static void WriteLineCentered(string text, bool changeColor = true)
+        {
+            int width = Console.WindowWidth;
+            int leftPadding = (width - text.Length) / 2;
+            if (leftPadding < 0)
+            {
+                leftPadding = 0;
+            }
+            Console.WriteLine(new string(' ', leftPadding) + text);
+        }
+        public static void WriteCentered(string text, bool changeColor = true)
+        {
+            int width = Console.WindowWidth;
+            int leftPadding = (width - text.Length) / 2;
+            if (leftPadding < 0)
+            {
+                leftPadding = 0;
+            }
+            Console.Write(new string(' ', leftPadding) + text);
         }
     }
 }
