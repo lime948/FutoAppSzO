e4c994e7391f1d63139683c2359b5148701b916b
lime948 <oreskoz@kkszki.hu>
2026-01-14 17:16:21 +0100

    Add project files.

---
+++ FutoApp/Models/User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FutoApp.Models
+{
+    class Training
+    {
+        public DateTime Datum { get; set; }
+        public double Tav { get; set; }
+        public TimeSpan Idotartam { get; set; }
+        public int MaxPulzus { get; set; }
+
+        public Training(DateTime datum, double tav, TimeSpan idotartam, int maxPulzus)
+        {
+            Datum = datum;
+            Tav = tav;
+            Idotartam = idotartam;
+            MaxPulzus = maxPulzus;
+        }
+    }
+    class Runner
+    {
+        public double Magassag { get; set; }
+        public double Testtomeg { get; set; }
+        public int NyugalmiPulzus { get; set; }
+        public TimeSpan CelIdo { get; set; }
+
+        public List<Training> Edzesek { get; set; }
+
+        public Runner(double magassag, double testtomeg, int nyugalmiPulzus, TimeSpan celIdo)
+        {
+            Magassag = magassag;
+            Testtomeg = testtomeg;
+            NyugalmiPulzus = nyugalmiPulzus;
+            CelIdo = celIdo;
+            Edzesek = new List<Training>();
+        }
+
+        public void UjEdzes(Training edzes)
+        {
+            Edzesek.Add(edzes);
+        }
+    }
+}
---
+++ FutoApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using FutoApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FutoApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            //Runner futo = new Runner(
+            //    magassag: 178,
+            //    testtomeg: 72,
+            //    nyugalmiPulzus: 55,
+            //    celIdo: new TimeSpan(0, 22, 0)
+            //);
+
+            //Training edzes1 = new Training(
+            //    datum: new DateTime(2026, 1, 10),
+            //    tav: 5.2,
+            //    idotartam: new TimeSpan(0, 27, 45),
+            //    maxPulzus: 178
+            //);
+
+            //futo.UjEdzes(edzes1);
+
+            //Console.WriteLine("Edzések száma: " + futo.Edzesek.Count);
+            //Console.WriteLine("Első edzés dátuma: " + futo.Edzesek[0].Datum.ToShortDateString());
+        }
+    }
+}
